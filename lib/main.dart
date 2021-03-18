69263334779dbb874a23145bd7dfc3b981a037d3
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-18 23:05:13 +0100

    url api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'user.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage () ,
+      home: UserList(),
     );
   }
 }
